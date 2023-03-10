327e60a6f23999a51c77ab58e051beb99e8fd023
Heikki Vihersalo <heikki@vihersalo.fi>
2023-03-10 16:33:17 +0200

    new build file

---
+++ build/theme/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7c7a5e84a234480c04e5');
--- build/theme/theme.asset.php
+++ build/theme/theme.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
+<?php return array('dependencies' => array(), 'version' => '8c5b220bf6f482881a90');
