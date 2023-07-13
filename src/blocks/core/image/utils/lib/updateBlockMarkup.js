dc964b76cc0e4fc016a60faf8ffd07983a508451
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-13 11:23:39 +0300

    classname fixes

--- src/blocks/core/image/utils/lib/updateBlockMarkup.js
+++ src/blocks/core/image/utils/lib/updateBlockMarkup.js
@@ -38,12 +38,30 @@ function updateBlockMarkup(element, blockType, attributes) {
         width,
         style
     } } = element.props.children.props.children[0];
+    
+    const getAlignmentClass = (align) => {
+        if (!align) return null;
 
+        switch (align) {
+            case 'left':
+                return 'has-align-left';
+            case 'right':
+                return 'has-align-right';
+            case 'center':
+                return 'has-align-center';
+            case 'wide':
+                return 'has-align-wide';
+            case 'full':
+                return 'has-align-full';
+            default:
+                return null;
+        }
+    }
     /** 
      * If image doesn't have figcaption, create image element
      */
     const imageBlock = createElement('img', {
-        className: classnames('wp-block-image', className, getBlockSizeClass(attributes?.sizeSlug)),
+        className: classnames('wp-block-image', className, attributes.className, getBlockSizeClass(attributes?.sizeSlug), getAlignmentClass(attributes?.align)),
         src,
         title,
         alt,
