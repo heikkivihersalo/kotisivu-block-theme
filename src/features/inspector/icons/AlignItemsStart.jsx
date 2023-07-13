a4927f42b964b0e04f924d4005d93106666de863
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-13 17:38:59 +0300

    initial commit

---
+++ src/features/inspector/icons/AlignItemsStart.jsx
@@ -0,0 +1,23 @@
+const AlignItemsStart = (props) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		xmlSpace="preserve"
+		style={{
+			fillRule: "evenodd",
+			clipRule: "evenodd",
+			strokeLinejoin: "round",
+			strokeMiterlimit: 2,
+		}}
+		viewBox="0 0 33 29"
+		{...props}
+	>
+		<path
+			d="M148.981 121.808v22.084h-6v-22.084h6Zm10.003 0v14.037h-6v-14.037h6Zm8.023-2.989h-32.036v-3.038h32.036v3.038Z"
+			style={{
+				fill: "currentColor",
+			}}
+			transform="translate(-134.971 -115.781)"
+		/>
+	</svg>
+);
+export { AlignItemsStart };
