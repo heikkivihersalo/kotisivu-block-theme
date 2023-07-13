3dd11131c93404db935317edadd456229d14a4c9
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-13 11:26:27 +0300

    initial commit

---
+++ src/features/inspector/lib/ariaLabelControls.js
@@ -0,0 +1,37 @@
+import { __ } from "@wordpress/i18n";
+import { TextControl, PanelBody } from '@wordpress/components';
+
+/**
+ * Controllers for aria-label and aria-labelledby
+ * Props ariaLabel and ariaLabelledBy must be defined in block attributes
+ *
+ * @param { object } { attributes, setAttributes }
+ * @return { JSX.Element } AriaLabelControls 
+ */
+const AriaLabelControls = ({ attributes, setAttributes }) => {
+    const {
+        ariaLabel,
+        ariaLabelledBy,
+    } = attributes;
+
+    return (
+        <>
+            <PanelBody title={__('Accessibility', 'kotisivu-block-theme')} initialOpen={false}>
+                <TextControl
+                    label={__('Aria Labelled By', 'kotisivu-block-theme')}
+                    help={__('Identifies the element (or elements) that labels the element it is applied to. Use the same ID as in the section heading.', 'kotisivu-block-theme')}
+                    value={ariaLabelledBy}
+                    onChange={(newAriaLabelledBy) => setAttributes({ ariaLabelledBy: newAriaLabelledBy })}
+                />
+                <TextControl
+                    label={__('Aria label', 'kotisivu-block-theme')}
+                    help={__('If section heading doesn`t match correct description, you can use aria-label. IMPORTANT! Use aria-labelledby by default.', 'kotisivu-block-theme')}
+                    value={ariaLabel}
+                    onChange={(newAriaLabel) => setAttributes({ ariaLabel: newAriaLabel })}
+                />
+            </PanelBody>
+        </>
+    )
+}
+
+export { AriaLabelControls }
---
+++ src/features/inspector/lib/backgroundColorControl.js
@@ -0,0 +1,40 @@
+import { __ } from "@wordpress/i18n";
+import { PanelColorSettings } from '@wordpress/block-editor';
+
+/**
+ * Wrapper for panelColorSettings
+ * Block style must be defined in block attributes
+ *
+ * @param { object } { attributes, setAttributes }
+ * @return { JSX.Element } PanelColorSettings 
+ */
+const BackgroundColorControl = ({ attributes, setAttributes }) => {
+    const {
+        style
+    } = attributes;
+
+    const onChangeBackgroundColor = (currentStyles, newBackgroundColor) => {
+        setAttributes({
+            style: {
+                ...currentStyles,
+                backgroundColor: newBackgroundColor
+            }
+        })
+    }
+
+    return (
+        <PanelColorSettings
+            title={__('Color settings', 'kotisivu-block-theme')}
+            initialOpen={false}
+            colorSettings={[
+                {
+                    value: style.backgroundColor,
+                    onChange: (newColor) => onChangeBackgroundColor(style, newColor),
+                    label: __('Background color', 'kotisivu-block-theme')
+                }
+            ]}
+        />
+    )
+}
+
+export { BackgroundColorControl }
