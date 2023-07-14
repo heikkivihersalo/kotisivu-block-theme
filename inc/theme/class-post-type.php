1a46d08e4a9b5ae2ad8fc972bcaa31a6163a74af
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-14 12:55:04 +0300

    added translations

--- inc/theme/class-post-type.php
+++ inc/theme/class-post-type.php
@@ -75,7 +75,33 @@ class CustomPostType {
         /* Guardclauses */
         if (!$post_type_config['active']) return;
 
-        $post_type = new PostTypes\PostType($post_type_config['names'], $post_type_config['options']);
+        $post_type = new PostTypes\PostType(
+            array(
+                'name' => $post_type_config['names']['name'],
+                'singular' => __($post_type_config['names']['singular'], 'kotisivu-block-theme'),
+                'plural' => __($post_type_config['names']['plural'], 'kotisivu-block-theme'),
+                'slug' => $post_type_config['names']['slug']
+            ),
+            $post_type_config['options']
+        );
+
+        /* Translate labels */
+        $post_type->labels(array(
+            'name' => __($post_type_config['labels']['name'], 'kotisivu-block-theme'),
+            'singular_name' => __($post_type_config['labels']['singular_name'], 'kotisivu-block-theme'),
+            'menu_name' => __($post_type_config['labels']['menu_name'], 'kotisivu-block-theme'),
+            'all_items' => __($post_type_config['labels']['all_items'], 'kotisivu-block-theme'),
+            'add_new' => __($post_type_config['labels']['add_new'], 'kotisivu-block-theme'),
+            'add_new_item' => __($post_type_config['labels']['add_new_item'], 'kotisivu-block-theme'),
+            'edit_item' => __($post_type_config['labels']['edit_item'], 'kotisivu-block-theme'),
+            'new_item' => __($post_type_config['labels']['new_item'], 'kotisivu-block-theme'),
+            'view_item' => __($post_type_config['labels']['view_item'], 'kotisivu-block-theme'),
+            'search_items' => __($post_type_config['labels']['search_items'], 'kotisivu-block-theme'),
+            'not_found' => __($post_type_config['labels']['not_found'], 'kotisivu-block-theme'),
+            'not_found_in_trash' => __($post_type_config['labels']['not_found_in_trash'], 'kotisivu-block-theme'),
+            'parent_item_colon' => __($post_type_config['labels']['parent_item_colon'], 'kotisivu-block-theme'),
+        ));
+
         $post_type->icon($post_type_config['icon']);
         $post_type->register();
 
