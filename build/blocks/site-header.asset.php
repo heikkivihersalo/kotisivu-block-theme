d571318d05daa15fe63eb2e5f4758692a5a6f025
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-18 11:58:30 +0300

    new build files

--- build/blocks/site-header.asset.php
+++ build/blocks/site-header.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd736fec8e638c873414f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6e97a6815c450fcd7eab');
