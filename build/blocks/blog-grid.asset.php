99ce8c383ff0478b9dd071fc94dbad7e183eadbe
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-18 13:47:04 +0300

    new build files

--- build/blocks/blog-grid.asset.php
+++ build/blocks/blog-grid.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'db99c09a114d1a2653c3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'bef5e7d3e49854371882');
