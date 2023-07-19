9c9dafd5061fe02d8c9971a5f3c9ea4fd8481172
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-19 18:41:13 +0300

    moved toggle directly to block

--- src/blocks/dynamic/site-header/render.php
+++ src/blocks/dynamic/site-header/render.php
@@ -5,7 +5,17 @@ function render_site_header($block_attributes, $content) { ?>
     <?php ob_start(); ?>
     <?php echo do_blocks('<!-- wp:ksd/site-logo {"className":"header__logo"} /-->'); ?>
     <nav class="header__site-nav">
-        <?php get_template_part('inc/template-parts/header', 'nav-toggle'); ?>
+        <button class="header__toggle" aria-controls="primary-navigation" aria-expanded="false">
+            <span class="is-visually-hidden"><?php echo __('Menu', 'kotisivu-block-theme'); ?></span>
+            <svg fill="var(--_toggle-color)" viewBox="0 0 100 100" width="50" aria-hidden="true">
+                <rect className="line top" width="80" height="10" x="10" y="25" rx="5">
+                </rect>
+                <rect className="line middle" width="80" height="10" x="10" y="45" rx="5">
+                </rect>
+                <rect className="line bottom" width="80" height="10" x="10" y="65" rx="5">
+                </rect>
+            </svg>
+        </button>
         <?php if (has_nav_menu('primary-navigation')) : ?>
             <?php wp_nav_menu(array(
                 'theme_location' => 'primary-navigation',
