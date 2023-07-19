1a53b8b8a23eac9d98903eb852a4891793ef15b3
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-19 18:41:03 +0300

    moved card code directly to block

--- src/blocks/dynamic/blog-grid/render.php
+++ src/blocks/dynamic/blog-grid/render.php
@@ -38,15 +38,50 @@ function render_blog_grid($block_attributes, $content) { ?>
     <?php ob_start(); ?>
 
     <div id="blog-post-container" class="blog-grid__container">
+        <?php
+        /**
+         * CONTAINER
+         * 
+         */ ?>
         <ul id="blog-post-list" class="blog-grid__list">
             <?php if ($query->have_posts()) : ?>
-
                 <?php while ($query->have_posts()) : $query->the_post(); ?>
-                    <?php get_template_part('blocks/src/templates/post', 'card'); ?>
-                <?php endwhile; ?>
+                    <?php
+                    /**
+                     * CARD
+                     * 
+                     */ ?>
+                    <li class="blog-grid__item" data-url="<?php echo esc_url(get_permalink()); ?>">
+                        <h3 class="blog-grid__title"><?php echo get_the_title(); ?></h3>
+                        <time class="blog-grid__date" datetime="<?php echo get_the_date('c'); ?>">
+                            <?php echo get_the_date(get_option('date_format')); ?>
+                        </time>
 
+                        <p class="blog-grid__excerpt"><?php echo get_the_excerpt(); ?></p>
+
+                        <?php $image_id = get_post_thumbnail_id(); ?>
+                        <?php if ($image_id) : ?>
+                            <?php $image_meta = wp_get_attachment_image_src($image_id, 'medium'); ?>
+                            <img class="blog-grid__image" src="<?php echo $image_meta[0]; ?>" alt="<?php echo get_post_meta($image_id, '_wp_attachment_image_alt', TRUE); ?>" title="<?php echo get_the_title($image_id); ?>" width="<?php echo $image_meta[1]; ?>" height="<?php echo $image_meta[2]; ?>">
+                        <?php else : ?>
+                            <img class="blog-grid__image" src="" />
+                        <?php endif; ?>
+
+                        <div class="blog-grid__button wp-block-button">
+                            <a class="wp-block-button__link wp-element-button" href="<?php echo esc_url(get_permalink()); ?>">
+                                <?php echo __('Read more', 'kotisivu-block-theme'); ?>
+                            </a>
+                        </div>
+                    </li>
+                <?php endwhile; ?>
             <?php endif; ?>
         </ul>
+
+        <?php
+        /**
+         * LOAD MORE BUTTON 
+         * 
+         */ ?>
         <?php echo $post_count->publish > 6 ? '<hr class="blog-grid__separator" />' : ''; ?>
         <div class="wp-block-button">
             <button class="wp-block-button__link wp-element-button" href="#!" id="blog-load-more" <?php echo $post_count->publish < 6 ? 'disabled' : ''; ?>>
