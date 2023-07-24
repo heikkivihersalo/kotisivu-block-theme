480db047f0968b2dec42d24be8afff97d39c71f8
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-24 11:47:56 +0300

    formatting, fixed template location

--- inc/blocks/class-block-ajax.php
+++ inc/blocks/class-block-ajax.php
@@ -13,7 +13,7 @@ class BlockAjax extends Blocks {
         /**
          * Check nonce
          */
-        check_ajax_referer( 'ajax-nonce', 'security' );
+        check_ajax_referer('ajax-nonce', 'security');
 
         /**
          * Init variables
@@ -44,12 +44,12 @@ class BlockAjax extends Blocks {
          * Append posts to response object
          */
         if ($query->have_posts()) :
-            while($query->have_posts()) : $query->the_post();
+            while ($query->have_posts()) : $query->the_post();
                 /**
-                 * Get current post content
+                 * Get current post content from 'blog grid' -block folder
                  */
                 ob_start();
-                get_template_part('blocks/src/templates/post', 'card');
+                get_template_part('src/blocks/dynamic/blog-grid/includes/card');
                 $post_content = ob_get_clean();
 
                 /**
@@ -60,7 +60,7 @@ class BlockAjax extends Blocks {
                     'date'      => get_the_date(get_option('date_format')),
                     'title'     => get_the_title(),
                     'excerpt'   => get_the_excerpt(),
-                    'thumbnail' => get_the_post_thumbnail_url(get_the_ID(),'full'),
+                    'thumbnail' => get_the_post_thumbnail_url(get_the_ID(), 'full'),
                     'url'       => esc_url(get_permalink()),
                     'content'   => $post_content
                 );
@@ -81,7 +81,7 @@ class BlockAjax extends Blocks {
     }
 
     public function init(): void {
-        add_action( 'wp_ajax_nopriv_load_more_posts', [$this, 'load_more_blog_posts'] );
-        add_action( 'wp_ajax_load_more_posts', [$this, 'load_more_blog_posts'] );
+        add_action('wp_ajax_nopriv_load_more_posts', [$this, 'load_more_blog_posts']);
+        add_action('wp_ajax_load_more_posts', [$this, 'load_more_blog_posts']);
     }
 }
