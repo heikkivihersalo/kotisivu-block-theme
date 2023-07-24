0c81580eefb451590856f5cb2f0ea4f1d97ea5f5
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-24 13:08:08 +0300

    fixed toggle closed state animation

--- src/blocks/dynamic/site-header/scripts/helpers.js
+++ src/blocks/dynamic/site-header/scripts/helpers.js
@@ -4,9 +4,7 @@
 1.0 Mobile Menu Toggle
   1.1 Add listener for opening mobile menu
   1.2 Add listener for closing mobile menu
-2.0 Sticky Header
-  2.1 Set class for sticky header
-3.0 Exports
+2.0 Exports
 --------------------------------------------------------------*/
 
 /*--------------------------------------------------------------
@@ -46,35 +44,7 @@ const closeMobileMenu = ({ header, toggle, links }) => {
 }
 
 /*--------------------------------------------------------------
-  2.0 Sticky Header
---------------------------------------------------------------*/
-/**
- * Set class for sticky header
- * @return void
- */
-const setStickyHeader = (elements) => {
-    const {
-        target,
-        isIntersecting
-    } = elements[0];
-    
-    if (isIntersecting) {
-        target.classList.remove("sticky");
-    } else {
-        target.classList.add("sticky");
-    }
-}
-
-const getStickyHeaderPadding = (scroll) => {
-    return scroll <= 100 ? 50 - (scroll / 4) : 25;
-}
-
-const getStickyHeaderWidth = (scroll) => {
-    return scroll <= 100 ? 30 - (scroll / 5) : 10;
-}
-
-/*--------------------------------------------------------------
-  3.0 Exports
+  2.0 Exports
 --------------------------------------------------------------*/
 
-export { openMobileMenu, closeMobileMenu, setStickyHeader, getStickyHeaderPadding, getStickyHeaderWidth }
+export { openMobileMenu, closeMobileMenu }
