e2104143f4c96782ae1dd618c25aca6815cf39d6
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-24 12:06:20 +0300

    new build files

--- build/blocks/section.asset.php
+++ build/blocks/section.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2ba605f857210f324490');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0ca499e73227d9bd6e51');
