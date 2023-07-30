e7f5347b42b856a520f009762c8bde652d78f4ce
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-30 14:17:36 +0300

    formatting

--- inc/theme/inline-css/custom.php
+++ inc/theme/inline-css/custom.php
@@ -9,18 +9,18 @@
     }
 
     /* Set global max width for content and center it to page */
-    main > .entry-content {
+    main>.entry-content {
         max-width: var(--wp--style--global--wide-size);
         margin-inline: auto;
     }
 
     /* Set default margin and padding excluding hero */
-    main > .entry-content > section:not(section:first-of-type) {
+    main>.entry-content>section:not(section:first-of-type) {
         margin-top: var(--wp--preset--spacing--60);
     }
 
     /* Set default margin and padding excluding last element */
-    main > .entry-content > section:not(section:last-of-type) {
+    main>.entry-content>section:not(section:last-of-type) {
         margin-bottom: var(--wp--preset--spacing--60);
     }
 </style>
