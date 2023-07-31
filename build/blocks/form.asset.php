552bd01ffda756d456040adbd991fb0774f563fc
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-31 15:55:04 +0300

    new build files

--- build/blocks/form.asset.php
+++ build/blocks/form.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '9b99b49000035e0618a3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd10c4477f715a1f78578');
--- build/blocks/hero.asset.php
+++ build/blocks/hero.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a18d8752eba75d299a0c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a183212eb20edcd8eac8');
