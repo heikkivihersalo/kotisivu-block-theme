02f80f8cdba49a2b52a9c892025d2174c6afd269
Heikki Vihersalo <heikki@vihersalo.fi>
2023-07-31 15:22:20 +0300

    reduced specifity

--- inc/theme/inline-css/custom.php
+++ inc/theme/inline-css/custom.php
@@ -1,6 +1,6 @@
 <style id="ksd-custom-inline-css">
     /* Set footer to be always on site bottom */
-    .wp-site-blocks {
+    :where(.wp-site-blocks) {
         min-height: 100vh;
         min-height: 100dvh;
 
@@ -9,18 +9,18 @@
     }
 
     /* Set global max width for content and center it to page */
-    main>.entry-content {
+    :where(main>.entry-content) {
         max-width: var(--wp--style--global--wide-size);
         margin-inline: auto;
     }
 
     /* Set default margin and padding excluding hero */
-    main>.entry-content>section:not(section:first-of-type) {
+    :where(main>.entry-content>section):not(section:first-of-type) {
         margin-top: var(--wp--preset--spacing--60);
     }
 
     /* Set default margin and padding excluding last element */
-    main>.entry-content>section:not(section:last-of-type) {
+    :where(main>.entry-content>section):not(section:last-of-type) {
         margin-bottom: var(--wp--preset--spacing--60);
     }
 
