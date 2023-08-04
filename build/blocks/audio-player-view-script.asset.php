4922f7e12eab1932ded8ba5d14a6eb9cf01fc925
Heikki Vihersalo <heikki@vihersalo.fi>
2023-08-04 17:41:10 +0300

    new build files

--- build/blocks/audio-player-view-script.asset.php
+++ build/blocks/audio-player-view-script.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'ad429a66086c48a0c9d1');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '345a9bea7f0b380453b9');
