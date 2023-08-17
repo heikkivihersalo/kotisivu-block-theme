cb6c6632983d15e1d9a9cde9a3e3ad52bcc35120
Heikki Vihersalo <heikki@vihersalo.fi>
2023-08-17 09:11:38 +0300

    new build files

---
+++ build/blocks/blog-post.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'c77a937cbf99f25a74ed');
--- build/blocks/dark-mode-toggle.asset.php
+++ build/blocks/dark-mode-toggle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'dd1ffff8ad56ff7a03aa');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd6e00bccd42ead7ea8e8');
