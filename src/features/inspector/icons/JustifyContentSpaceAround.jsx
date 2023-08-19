4e8ebbd2cbf7f015e91aff7d1db39e6b9e23eb9a
Heikki Vihersalo <heikki@vihersalo.fi>
2023-08-19 16:40:53 +0300

    added justifyContent and alignContent icons

---
+++ src/features/inspector/icons/JustifyContentSpaceAround.jsx
@@ -0,0 +1,22 @@
+const JustifyContentSpaceAround = (props) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		xmlSpace="preserve"
+		style={{
+			fillRule: "evenodd",
+			clipRule: "evenodd",
+			strokeLinejoin: "round",
+			strokeMiterlimit: 2,
+		}}
+		viewBox="0 0 196 196"
+		{...props}
+	>
+		<path
+			d="M64.618,47.53L64.618,143L28.087,143L28.087,47.53L64.618,47.53ZM167.373,47.53L167.373,143L130.841,143L130.841,47.53L167.373,47.53ZM18.565,0L18.565,195.765L0,195.765L0,0L18.565,0ZM176.895,195.765L176.895,0L195.459,0L195.459,195.765L176.895,195.765Z"
+			style={{
+				fill: "currentColor",
+			}}
+		/>
+	</svg>
+);
+export { JustifyContentSpaceAround };
