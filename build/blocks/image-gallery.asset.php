1e369ed7b5134acc1d3efa3925f286b33b1f523b
Heikki Vihersalo <heikki@vihersalo.fi>
2023-09-04 17:18:17 +0300

    new build files

--- build/blocks/image-gallery.asset.php
+++ build/blocks/image-gallery.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '11d21b56fdf0f09e0f82');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '27c20ca74a792e5d730e');
