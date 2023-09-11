d81cc7cc084a9a75b1a3975327a3cadac6c0b464
Heikki Vihersalo <heikki@vihersalo.fi>
2023-09-11 14:33:38 +0300

    new build files

--- build/blocks/wrapper.asset.php
+++ build/blocks/wrapper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'da24dd904c85ac293500');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '0f82336febe05e2009ab');
