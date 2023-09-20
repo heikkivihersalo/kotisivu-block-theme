d7d20af43d765c499719f7b6fee5b1dcecbc3331
Heikki Vihersalo <heikki@vihersalo.fi>
2023-09-20 13:01:10 +0300

    disable rest for non-logged in users, author pages

--- inc/theme/class-filters.php
+++ inc/theme/class-filters.php
@@ -210,6 +210,47 @@ class Filters extends Theme {
     }
 
     /**
+     * Disable REST API for non logged in users
+     * @param mixed $result
+     * @return mixed
+     * @link https://developer.wordpress.org/rest-api/using-the-rest-api/frequently-asked-questions/#require-authentication-for-all-requests
+     */
+    public function disable_rest_api_for_non_logged_in_users($result) {
+        // If a previous authentication check was applied,
+        // pass that result along without modification.
+        if (true === $result || is_wp_error($result)) {
+            return $result;
+        }
+
+        // No authentication has been performed yet.
+        // Return an error if user is not logged in.
+        if (!is_user_logged_in()) {
+            return new WP_Error(
+                'rest_not_logged_in',
+                __('You are not currently logged in.'),
+                array('status' => 401)
+            );
+        }
+
+        // Our custom authentication check should have no effect
+        // on logged-in requests
+        return $result;
+    }
+
+    /**
+     * Disable author pages
+     * @return void
+     */
+    public function disable_author_pages() {
+        global $wp_query;
+
+        if (is_author()) {
+            wp_redirect(get_option('home'), 301);
+            exit;
+        }
+    }
+
+    /**
      * Initialize class
      * @return void 
      */
@@ -223,5 +264,7 @@ class Filters extends Theme {
         add_filter('wp_enqueue_scripts', [$this, 'remove_jquery']);
         add_action('rest_api_init', [$this, 'register_rest_images_for_posts']);
         add_action('rest_api_init', [$this, 'register_rest_images_for_media']);
+        add_filter('rest_authentication_errors', [$this, 'disable_rest_api_for_non_logged_in_users']);
+        add_action('template_redirect', [$this, 'disable_author_pages']);
     }
 }
