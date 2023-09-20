d0fe1804727ae0386abcf35276fe6687ccf9d5d3
Heikki Vihersalo <heikki@vihersalo.fi>
2023-09-20 12:35:26 +0300

    editor fixes

--- assets/js/blocks/hero.asset.php
+++ assets/js/blocks/hero.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b96fff54df3ec0370b8d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3aae7bc7dbbda3d09a5d');
--- assets/js/blocks/wrapper.asset.php
+++ assets/js/blocks/wrapper.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'a84fc927a11cc6092966');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'e3b04dfdf19d608974f1');
