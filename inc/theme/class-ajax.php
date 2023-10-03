843a9111606e33c0ded47fb8f7b9d7b52a04092a
Heikki Vihersalo <heikki@vihersalo.fi>
2023-10-03 11:13:54 +0300

    added ajax class

---
+++ inc/theme/class-ajax.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kotisivu\BlockTheme;
+
+defined('ABSPATH') or die();
+
+/**
+ * 
+ * 
+ * Inherits following attributes
+ * * name
+ * * version
+ * * textdomain
+ * * options
+ * * config
+ * * path
+ * * uri
+ * * parent_path
+ * * parent_uri
+ * 
+ * @package Kotisivu\BlockTheme
+ */
+class Ajax extends Theme {
+    public function init() {
+    }
+}
