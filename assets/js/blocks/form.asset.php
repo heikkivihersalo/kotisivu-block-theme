126c3b141ad3191e34d87f21a7ec9ac6b3c82e11
Heikki Vihersalo <heikki@vihersalo.fi>
2023-11-09 13:21:25 +0200

    new build files

--- assets/js/blocks/form.asset.php
+++ assets/js/blocks/form.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a5ca65c1ab77b3ac38cc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4465437f0d051756e357');
