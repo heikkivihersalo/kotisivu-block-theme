5c6383836fe2d01166fcbe5562bfda10f5c10101
Heikki Vihersalo <heikki@vihersalo.fi>
2023-11-23 12:18:49 +0200

    new build files

--- assets/js/blocks/site-social.asset.php
+++ assets/js/blocks/site-social.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '84bfb39ef28e195daf1a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '526fae9b69d26822699e');
