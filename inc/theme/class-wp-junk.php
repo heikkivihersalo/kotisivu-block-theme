305448c0e909cfb2be0c4deea7ea6ff3aec898f9
Heikki Vihersalo <heikki@vihersalo.fi>
2023-11-23 13:41:27 +0200

    added other junk section

--- inc/theme/class-wp-junk.php
+++ inc/theme/class-wp-junk.php
@@ -37,6 +37,47 @@ class Junk {
     }
 
     /**
+     * Disable REST API for non logged in users
+     * @param mixed $result
+     * @return mixed
+     * @link https://developer.wordpress.org/rest-api/using-the-rest-api/frequently-asked-questions/#require-authentication-for-all-requests
+     */
+    public function disable_rest_api_for_non_logged_in_users($result) {
+        // If a previous authentication check was applied,
+        // pass that result along without modification.
+        if (true === $result || is_wp_error($result)) {
+            return $result;
+        }
+
+        // No authentication has been performed yet.
+        // Return an error if user is not logged in.
+        if (!is_user_logged_in()) {
+            return new WP_Error(
+                'rest_not_logged_in',
+                __('You are not currently logged in.'),
+                array('status' => 401)
+            );
+        }
+
+        // Our custom authentication check should have no effect
+        // on logged-in requests
+        return $result;
+    }
+
+    /**
+     * Disable author pages
+     * @return void
+     */
+    public function disable_author_pages() {
+        global $wp_query;
+
+        if (is_author()) {
+            wp_redirect(get_option('home'), 301);
+            exit;
+        }
+    }
+
+    /**
      * Disable emojis
      * @return void
      */
@@ -285,11 +326,25 @@ class Junk {
     }
 
     /**
+     * Remove other junk from Wordpress
+     * @return void
+     */
+    public function remove_other_junk(): void {
+        if ($this->settings['otherJunk']['unauthorized-rest-api']) {
+            add_filter('rest_authentication_errors', [$this, 'disable_rest_api_for_non_logged_in_users']);
+        }
+        if ($this->settings['otherJunk']['author-pages']) {
+            add_action('template_redirect', [$this, 'disable_author_pages']);
+        }
+    }
+
+    /**
      * Initialize class
      * @return void
      */
     public function init(): void {
         $this->remove_header_junk();
         $this->remove_style_junk();
+        $this->remove_other_junk();
     }
 }
