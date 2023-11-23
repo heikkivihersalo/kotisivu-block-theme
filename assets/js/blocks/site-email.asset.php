8193863ab7485ee0519ec04878c39baa8cd63687
Heikki Vihersalo <heikki@vihersalo.fi>
2023-11-23 12:10:24 +0200

    new build files

--- assets/js/blocks/site-email.asset.php
+++ assets/js/blocks/site-email.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'bfb83130563688bd9158');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '3a26b5f1740a5090a544');
--- assets/js/blocks/site-phone.asset.php
+++ assets/js/blocks/site-phone.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '2fe7e9ae46049c8a63e7');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'cd5be8fe55d356063bda');
