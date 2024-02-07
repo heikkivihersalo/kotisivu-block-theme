9d54fff8715c87bfc22143f8ca277e543e9a3438
Heikki Vihersalo <heikki@vihersalo.fi>
2024-02-07 16:11:14 +0200

    migrated to new folder structure

--- inc/blocks/class-block-custom.php
+++ inc/blocks/class-block-custom.php
@@ -36,10 +36,16 @@ class BlockCustom {
     protected $uri;
 
     /**
+     * Type
+     * @var string
+     */
+    protected $type;
+
+    /**
      * Constructor
      * @return void 
      */
-    public function __construct($blocks, $parent_path, $parent_uri, $path, $uri) {
+    public function __construct($blocks, $parent_path, $parent_uri, $path, $uri, $type = 'custom') {
         /**
          * Get classes
          */
@@ -54,6 +60,7 @@ class BlockCustom {
         $this->parent_uri = $parent_uri;
         $this->path = $path;
         $this->uri = $uri;
+        $this->type = $type;
     }
 
     /**
@@ -62,17 +69,20 @@ class BlockCustom {
      */
     public function register_custom_blocks(): void {
         foreach ($this->blocks as $block) :
-            try {
-                $block_path = Utils::get_block_path($block, 'custom', $this->path, $this->parent_path);
-                register_block_type($block_path);
+            // Get block path
+            $block_path = Utils::get_block_path($block, $this->type, $this->path, $this->parent_path);
 
-                Utils::set_block_translation(
-                    'ksd-' . explode('/', $block)[1],
-                    $this->parent_path
-                );
-            } catch (\Exception $e) {
-                Utils::write_log($e->getMessage());
-            }
+            // Trigger error if deprecated function is used
+            add_filter('doing_it_wrong_trigger_error', function ($trigger, $function, $message, $version) use ($block, $block_path) {
+                throw new \Exception("Trying to register block {$block} but failed for some reason.\n\nThis can be caused of non-existing block in block.json. Make sure that preceding and next blocks in block.json list exists.\n\n Message: {$message}\n");
+            }, 10, 4);
+
+            // Register block
+            register_block_type($block_path);
+            Utils::set_block_translation(
+                'ksd-' . explode('/', $block)[1],
+                $this->parent_path
+            );
 
         endforeach;
     }
