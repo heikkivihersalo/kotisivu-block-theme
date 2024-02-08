3d6225591e271bebf98210e4b3661c02f82ecae1
Heikki Vihersalo <heikki@vihersalo.fi>
2024-02-08 11:51:24 +0200

    updated database code

--- inc/theme/class-database.php
+++ inc/theme/class-database.php
@@ -34,37 +34,74 @@ class Database {
      * @return void
      */
     public function create_database_tables(): void {
+        global $wpdb;
+
         /**
          * Guard clauses
          */
-        if (get_option('kotisivu-block-theme_first_theme_activation') === true) return;
+        // if (get_option('kotisivu-block-theme_first_theme_activation') === true) return;
 
         /**
          * Create new table
          */
-        global $wpdb;
+        $query = [];
 
         /**
-         * Loop over custom database tables
+         * Add tables
          */
-        foreach ($this->tables as $table) {
+        foreach ($this->tables as $table) :
             $name = $wpdb->prefix . $table['name'];
-            $schema = $this->get_schema($table['schema']);
-
-            /**
-             * Set SQL query
-             */
+            $schema = $this->get_schema($table['schema'], $wpdb->prefix);
             $charset_collate = $wpdb->get_charset_collate();
-            $sql = "CREATE TABLE $name ($schema) $charset_collate;";
+            $query[] = array(
+                'table_name' => $name,
+                'query' => "CREATE TABLE IF NOT EXISTS $name ($schema) $charset_collate"
+            );
+        endforeach;
 
-            var_dump($sql);
+        /**
+         * Run query
+         */
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+        foreach ($query as $sql) :
+            $table_name = $sql['table_name'];
+            $query = $sql['query'];
 
             /**
-             * Run SQL query
+             * Check if table already exists
              */
-            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-            dbDelta($sql);
-        }
+            if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") == $sql['table_name']) {
+                add_action('admin_notices', function () use ($table_name) {
+                    echo '<div class="notice notice-warning is-dismissible">
+                             <p>' . sprintf(__('Table %1$s already exists', 'kotisivu-block-theme'), esc_html($table_name)) . '</p>
+                        </div>';
+                });
+                continue;
+            }
+
+            /**
+             * Create table if not exists
+             */
+            dbDelta($query);
+
+            /**
+             * Check if table was created succesfully
+             */
+            if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") == $sql['table_name']) {
+                add_action('admin_notices', function () use ($table_name) {
+                    echo '<div class="notice notice-success is-dismissible">
+                             <p>' . sprintf(__('Table %1$s created succesfully', 'kotisivu-block-theme'), esc_html($table_name)) . '</p>
+                        </div>';
+                });
+            } else {
+                add_action('admin_notices', function () use ($table_name) {
+                    echo '<div class="notice notice-error is-dismissible">
+                             <p>' . sprintf(__('Table %1$s creation failed', 'kotisivu-block-theme'), esc_html($table_name)) . '</p>
+                        </div>';
+                });
+            }
+        endforeach;
 
         /**
          * Update database version and first theme activation status
@@ -78,11 +115,16 @@ class Database {
      * @param array $schema
      * @return string
      */
-    public function get_schema($schema): string {
+    public function get_schema($schema, $prefix): string {
         /**
          * Set SQL query
          */
         $sql = '';
+
+        /**
+         * Store foreign keys to temporary array
+         */
+        $foreign_keys = [];
 
         /**
          * Loop over schema
@@ -99,10 +141,21 @@ class Database {
             foreach ($properties as $property => $value) :
                 switch ($property):
                     case 'type':
+                        // Default boolean to bit(1)
+                        if ($value === 'boolean') {
+                            $value = 'bit(1)';
+                        };
+                        // Other types
                         $sql .= ' ' . $value;
                         break;
                     case 'length':
                         $sql .= '(' . $value . ') ';
+                        break;
+                    case 'unsigned':
+                        $sql .= $value ? ' UNSIGNED' : '';
+                        break;
+                    case 'unique':
+                        $sql .= $value ? ' UNIQUE' : '';
                         break;
                     case 'not_null':
                         $sql .= $value ? ' NOT NULL' : '';
@@ -116,8 +169,16 @@ class Database {
                         break;
                     case 'primary_key':
                         if ($value) {
-                            $primary_key = 'PRIMARY KEY ' . '(' . $properties['name'] . ')';
+                            $primary_key = 'PRIMARY KEY  ' . '(' . $properties['name'] . ')';
                         }
+                        break;
+                    case 'foreign_key':
+                        $key = 'FOREIGN KEY  ' . '(' . $properties['name'] . ')';
+                        $reference = 'REFERENCES ' . $prefix . $value['table'] . '(' . $value['column'] . ')';
+                        $on_delete = isset($value['on_delete']) ? 'ON DELETE ' . $value['on_delete'] : '';
+                        $on_update = isset($value['on_update']) ? 'ON UPDATE ' . $value['on_update'] : '';
+
+                        $foreign_keys[] = $key . ' ' . $reference . ' ' . $on_delete . ' ' . $on_update;
                         break;
                     default:
                         break;
@@ -135,6 +196,17 @@ class Database {
          */
         if ($primary_key) {
             $sql .= ' ' . $primary_key;
+        }
+
+        /**
+         * Add foreign key to SQL query
+         */
+
+        if (count($foreign_keys) > 0) {
+            foreach ($foreign_keys as $key) {
+                $sql .= ', ';
+                $sql .= $key;
+            }
         }
 
         return $sql;
