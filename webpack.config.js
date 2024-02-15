288acd0b02189be8284d6985fef59e0ac7dc6481
Heikki Vihersalo <heikki@vihersalo.fi>
2024-02-15 11:25:14 +0200

    cleaned and simplified configs

--- webpack.config.js
+++ webpack.config.js
@@ -8,69 +8,31 @@
 >>> TABLE OF CONTENTS:
 ----------------------------------------------------------------
 1.0 Dependencies
-2.0 Constants
-3.0 Helper functions
-4.0 Webpack configurations
+2.0 Helper functions
+3.0 Webpack configurations
 --------------------------------------------------------------*/
 
 /*--------------------------------------------------------------
   1.0 Dependencies
 --------------------------------------------------------------*/
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 const glob = require('glob');
 
 // Import the @wordpress/scripts config.
-const defaultConfig = require('@wordpress/scripts/config/webpack.config')
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
 // Import the utility to auto-generate the entry points in the src directory.
-const { getWebpackEntryPoints } = require('@wordpress/scripts/utils/config')
+const { getWebpackEntryPoints } = require('@wordpress/scripts/utils/config');
 
 /*--------------------------------------------------------------
-  2.0 Constants
+  2.0 Helper functions
 --------------------------------------------------------------*/
-const BLOCK_SOURCE_PATH = './src/blocks';
-const THEME_SOURCE_PATH = './src/theme/scripts';
-
-/*--------------------------------------------------------------
-  3.0 Helper functions
---------------------------------------------------------------*/
-/**
- * Override the default WordPress plugins config.
- * @param {array} plugins
- * @returns {array}
- */
-const getPlugins = (plugins) => {
-    plugins.forEach((p, i) => {
-        /**
-         * Override the default CleanWebpackPlugin config.
-         */
-        if (p.constructor.name === 'CleanWebpackPlugin') {
-            plugins.splice(i, 1, new CleanWebpackPlugin({
-                ...p,
-                cleanOnceBeforeBuildPatterns: [
-                    '**/*',
-                    '!images/**',
-                    '!theme/**'
-                ],
-                cleanAfterEveryBuildPatterns: ['!images/**', '!theme/**']
-            }));
-        }
-    })
-
-    return plugins;
-}
-
 /**
  * Get all core blocks from source folder
  * @returns {array}
  */
 function getCoreBlocks() {
-    const src = glob.sync(BLOCK_SOURCE_PATH + "/core/**/*.js");
+    const src = glob.sync("./src/blocks/core/**/*.js");
     const blocks = [];
 
     src.forEach(entry => {
@@ -87,83 +49,25 @@ function getCoreBlocks() {
 };
 
 /*--------------------------------------------------------------
-  4.0 Webpack configurations
+  3.0 Webpack configurations
 --------------------------------------------------------------*/
-module.exports = [
-    //
-    // Extend the default WordPress config.
-    //
-    {
-        ...defaultConfig,
-        entry: {
-            ...getWebpackEntryPoints(),
-            'blocks/core/core': getCoreBlocks(),
-        },
-        output: {
-            path: path.resolve(process.cwd(), 'build'),
-        },
-        resolve: {
-            alias: {
-                '@features': path.resolve('src/features'),
-                '@utils': path.resolve('src/utils'),
-                '@hooks': path.resolve('src/hooks')
-            }
-        },
-        plugins: getPlugins(defaultConfig.plugins),
+module.exports = {
+    ...defaultConfig,
+    entry: {
+        ...getWebpackEntryPoints(),
+        'blocks/core/core': getCoreBlocks(),
+        'theme/admin': path.resolve(__dirname, 'src/theme/scripts/admin.js'),
+        'theme/cpt': path.resolve(__dirname, 'src/theme/scripts/cpt.js'),
+        'theme/dark-mode': path.resolve(__dirname, 'src/theme/scripts/dark-mode.js'),
+        'theme/inline': path.resolve(__dirname, 'src/theme/scripts/inline.js'),
+        'theme/sanitize': path.resolve(__dirname, 'src/theme/scripts/sanitize.js'),
+        'theme/theme': path.resolve(__dirname, 'src/theme/scripts/theme.js'),
     },
-    //
-    // Theme config.
-    //
-    {
-        entry: {
-            'theme': [THEME_SOURCE_PATH + '/theme.js'],
-            'admin': THEME_SOURCE_PATH + '/admin.js',
-            'inline': THEME_SOURCE_PATH + '/inline.js',
-            'sanitize': THEME_SOURCE_PATH + '/sanitize.js',
-            'dark-mode': THEME_SOURCE_PATH + '/dark-mode.js'
-        },
-        output: {
-            filename: 'theme/[name].js',
-            path: path.resolve(process.cwd(), 'build')
-        },
-        plugins: [
-            new CleanWebpackPlugin({
-                cleanOnceBeforeBuildPatterns: [
-                    'theme/*'
-                ],
-            }),
-            new MiniCssExtractPlugin({
-                filename: 'theme/[name].css'
-            }),
-            new DependencyExtractionWebpackPlugin()
-        ].filter(Boolean),
-        module: {
-            rules: [
-                {
-                    test: /\.css$/i,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        'css-loader'
-                    ]
-                },
-                {
-                    test: /\.js$|jsx/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ['@wordpress/babel-preset-default']
-                        }
-                    }
-                }
-            ]
-        },
-        optimization: {
-            minimize: true,
-            minimizer: [
-                new CssMinimizerPlugin(),
-                new TerserPlugin(),
-            ],
-        },
+    resolve: {
+        alias: {
+            '@features': path.resolve('src/features'),
+            '@utils': path.resolve('src/utils'),
+            '@hooks': path.resolve('src/hooks')
+        }
     }
-]
+}
