f90eb469b9d5772c9ee107d2c653e73a4f9db17b
Heikki Vihersalo <heikki@vihersalo.fi>
2024-02-27 17:03:15 +0200

    moved to parts

---
+++ src/blocks/parts/part-logo/edit.js
@@ -0,0 +1,24 @@
+import { __ } from "@wordpress/i18n";
+import { useBlockProps } from "@wordpress/block-editor";
+import ServerSideRender from '@wordpress/server-side-render';
+import Inspector from "./components/Inspector";
+import './editor.css';
+
+const Edit = (props) => {
+	const blockProps = useBlockProps({
+		className: "site-logo-editor-wrapper"
+	});
+
+	return (
+		<div {...blockProps}>
+			<Inspector {...props} />
+			<ServerSideRender
+				block="ksd/part-logo"
+				attributes={props.attributes}
+				className={props.className}
+			/>
+		</div>
+	);
+};
+
+export default Edit;
