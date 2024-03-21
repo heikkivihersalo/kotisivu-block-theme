7475c8a17803a3810debd797ba3cc789d0422dfd
Heikki Vihersalo <heikki@vihersalo.fi>
2024-03-21 17:07:24 +0200

    new build files

--- build/blocks/parts/part-header/index.asset.php
+++ build/blocks/parts/part-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f131cfacf86879fa8eb9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd2a04ecf1fd4b464fecf');
