8db7021d4403740e4811fda64a7a3cd4fc61c4cb
Heikki Vihersalo <heikki@vihersalo.fi>
2024-03-28 20:31:41 +0200

    new build files

--- build/blocks/__block-examples__/blog-grid/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-i18n'), 'version' => '299f786b64f03d83f1d4');
---
+++ build/blocks/templates/template-home/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-i18n'), 'version' => '2896bb37fb7af4352931');
