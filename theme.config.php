a37b04f70b956d16f5012c51d0da764435d68c1d
Heikki Vihersalo <heikki@vihersalo.fi>
2024-03-28 21:14:21 +0200

    added menu items and dashboard to config

--- theme.config.php
+++ theme.config.php
@@ -45,6 +45,87 @@ return array(
 			"rest-api-user-endpoints" => false,
 			"unauthorized-rest-api" => false,
 			"author-pages" => true
+		],
+		"removeAdminBarJunk" => [
+			"wp-logo" => [
+				"remove" => true,
+				"children" => [
+					"about" => true,
+					"wporg" => true,
+					"documentation" => true,
+					"support-forum" => true,
+					"feedback" => true
+				]
+			],
+			"site-name" => [
+				"remove" => false,
+				"children" => [
+					"dashboard" => false,
+					"themes" => true,
+					"menus" => true,
+				]
+			],
+			"site-editor" => [
+				"remove" => true,
+				"children" => []
+			],
+			"customize" => [
+				"remove" => false,
+				"children" => []
+			],
+			"comments" => [
+				"remove" => true,
+				"children" => []
+			],
+			"new-content" => [
+				"remove" => true,
+				"children" => []
+			],
+			"new-post" => [
+				"remove" => true,
+				"children" => []
+			],
+			"new-media" => [
+				"remove" => true,
+				"children" => []
+			],
+			"new-page" => [
+				"remove" => true,
+				"children" => []
+			],
+			"new-user" => [
+				"remove" => true,
+				"children" => []
+			],
+			"edit" => [
+				"remove" => true,
+				"children" => []
+			],
+			"user-actions" => [ // to the right - next to your avatar image
+				"remove" => false,
+				"children" => [
+					"user-info" => false,
+					"edit-profile" => false,
+					"logout" => false
+				]
+			],
+			"search" => [
+				"remove" => true,
+				"children" => []
+			],
+			"llar-root" => [ // Limit Login Attempts Reloaded Plugin
+				"remove" => true,
+				"children" => []
+			],
+		],
+		"removeDashboardJunk" => [
+			'dashboard_site_health',
+			'dashboard_right_now',
+			'dashboard_activity',
+			'dashboard_quick_press',
+			'dashboard_primary',
+			'llar_stats_widget', // Limit Login Attempts Reloaded plugin
+			'fluentform_stat_widget' // Fluent Forms plugin
 		]
 	],
 	"menus" => [
