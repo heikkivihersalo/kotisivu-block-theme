c98f3e9c97e89cdd8513aaf9285fed47819d53e4
Heikki Vihersalo <heikki@vihersalo.fi>
2024-04-04 12:38:33 +0300

    variation picker rewrite

--- src/components/variations/components/variationPicker.jsx
+++ src/components/variations/components/variationPicker.jsx
@@ -1,12 +1,40 @@
 /**
  * WordPress dependencies
  */
-import {__} from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 // eslint-disable-next-line @wordpress/no-unsafe-wp-apis
-import {__experimentalBlockVariationPicker as BlockVariationPicker} from '@wordpress/block-editor';
-import {getAttributesFromProps} from '../utils';
+import { __experimentalBlockVariationPicker as BlockVariationPicker } from '@wordpress/block-editor';
 
-const VariationPicker = ({setAttributes, blockVariations}) => {
+/**
+ * Internal dependencies
+ */
+import { useBlockVariations } from '@hooks';
+
+/**
+ * Build attributes from variation props
+ * @param {Object} variation
+ * @return {Object} attributes
+ */
+function getAttributesFromProps(variation) {
+	return {
+		variationName: variation?.name,
+		template: variation?.innerBlocks,
+		childTemplate: variation.attributes?.childTemplate,
+		blockClass: variation.attributes?.blockClass,
+		style: variation.attributes?.style,
+	};
+}
+
+/**
+ * Variation picker component
+ * @param {Object} props Component properties
+ * @param {string} props.blockName Block name
+ * @param {Function} props.setAttributes Set attributes function
+ * @return {JSX.Element} Variation picker component
+ */
+function VariationPicker({ blockName, setAttributes }) {
+	const blockVariations = useBlockVariations(blockName);
+
 	if (!blockVariations) {
 		return null;
 	}
@@ -28,6 +56,6 @@ const VariationPicker = ({setAttributes, blockVariations}) => {
 			/>
 		</>
 	);
-};
+}
 
-export {VariationPicker};
+export { VariationPicker };
--- src/components/variations/utils/index.js
+++ src/components/variations/utils/index.js
@@ -1,36 +1,14 @@
-import {
-    store as blocksStore,
-} from '@wordpress/blocks';
-
-import { useSelect } from '@wordpress/data';
-
-/**
- * Get block variations
- * @param {string} blockName
- * @return {Array} blockVariations
- */
-// TODO: Refactor this to React standards
-export const getBlockVariations = (blockName) => {
-    return useSelect(
-        (select) => {
-            const { getBlockVariations } = select(blocksStore);
-            return getBlockVariations(blockName, 'block');
-        },
-        [blockName]
-    )
-}
-
 /**
  * Build attributes from variation props
  * @param {Object} variation
  * @return {Object} attributes
  */
 export const getAttributesFromProps = (variation) => {
-    return {
-        variationName: variation?.name,
-        template: variation?.innerBlocks,
-        childTemplate: variation.attributes?.childTemplate,
-        blockClass: variation.attributes?.blockClass,
-        style: variation.attributes?.style
-    }
-}
+	return {
+		variationName: variation?.name,
+		template: variation?.innerBlocks,
+		childTemplate: variation.attributes?.childTemplate,
+		blockClass: variation.attributes?.blockClass,
+		style: variation.attributes?.style,
+	};
+};
