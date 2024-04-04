032cb2e0db932685a94c70aafecc25beb01c0e95
Heikki Vihersalo <heikki@vihersalo.fi>
2024-04-04 12:36:37 +0300

    new build files

--- build/block-library/custom/hero/index.asset.php
+++ build/block-library/custom/hero/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '2641697ef4513dda3872');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd705c617e42acd845ca5');
