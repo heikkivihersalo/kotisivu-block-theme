6c58a4bf1c58e89105aa2b8a67244adf57bceb0b
Heikki Vihersalo <heikki@vihersalo.fi>
2024-05-02 10:17:13 +0300

    new build files

--- build/block-library/custom/form/index.asset.php
+++ build/block-library/custom/form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '3cbbf0aba7621d415a60');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '4da67b14f695b516dddc');
