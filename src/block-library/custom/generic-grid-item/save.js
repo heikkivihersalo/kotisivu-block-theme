8142c0349878e05b597acf52edf390413c68547f
Heikki Vihersalo <heikki@vihersalo.fi>
2024-05-02 09:59:15 +0300

    initial commit

---
+++ src/block-library/custom/generic-grid-item/save.js
@@ -0,0 +1,23 @@
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+
+/**
+ * Block save function
+ * @param {Object} props Properties
+ * @return {JSX.Element} Block innerBlocks markup
+ */
+export default function Save(props) {
+	const {
+		attributes: { blockClass },
+	} = props;
+
+	const innerBlocksProps = useInnerBlocksProps.save(
+		useBlockProps.save({
+			className: blockClass,
+		})
+	);
+
+	return <div {...innerBlocksProps} />;
+}
