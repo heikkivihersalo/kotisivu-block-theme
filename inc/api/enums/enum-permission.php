24c648ba0beb4616f35e3a68f8aa21998ea1da26
Heikki Vihersalo <heikki@vihersalo.fi>
2024-05-19 11:46:24 +0300

    exmples for permission callback enums

--- inc/api/enums/enum-permission.php
+++ inc/api/enums/enum-permission.php
@@ -10,14 +10,40 @@ defined('ABSPATH') or die();
  * @package Kotisivu\BlockTheme\Api
  */
 enum Permission {
+    /**
+     * Admin permission
+     */
     case ADMIN;
+    /**
+     * Uncomment this to add new permission
+     */
+    // case EXAMPLE_ROLE;
+
+    /**
+     * Public permission for everyone
+     */
     case PUBLIC;
 
     public function get_callback(): \Closure|bool {
         return match ($this) {
             self::ADMIN => function () {
-                return current_user_can('manage_options');
+                $user = wp_get_current_user();
+
+                if (in_array('administrator', (array) $user->roles)) {
+                    return true;
+                }
             },
+            /**
+             * Uncomment this to add new permission
+             * Remember to update the values to match your needs
+             */
+            // self::EXAMPLE_ROLE => function () {
+            //     $user = wp_get_current_user();
+
+            //     if (in_array('example', (array) $user->roles)) {
+            //         return true;
+            //     }
+            // },
             self::PUBLIC => function () {
                 return true;
             },
