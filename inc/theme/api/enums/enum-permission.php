5e8a80efcd9aab662ffddc381f73a2951e830211
Heikki Vihersalo <heikki@vihersalo.fi>
2024-06-20 12:03:27 +0300

    moved to theme folder

---
+++ inc/theme/api/enums/enum-permission.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Kotisivu\BlockTheme\Api;
+
+defined('ABSPATH') or die();
+
+/**
+ * Enum Permission
+ * 
+ * @package Kotisivu\BlockTheme\Api
+ */
+enum Permission {
+    /**
+     * Admin permission
+     */
+    case ADMIN;
+    /**
+     * Uncomment this to add new permission
+     */
+    // case EXAMPLE_ROLE;
+
+    /**
+     * Public permission for everyone
+     */
+    case PUBLIC;
+
+    public function get_callback(): \Closure|bool {
+        return match ($this) {
+            self::ADMIN => function () {
+                $user = wp_get_current_user();
+
+                if (in_array('administrator', (array) $user->roles)) {
+                    return true;
+                }
+            },
+            /**
+             * Uncomment this to add new permission
+             * Remember to update the values to match your needs
+             */
+            // self::EXAMPLE_ROLE => function () {
+            //     $user = wp_get_current_user();
+
+            //     if (in_array('example', (array) $user->roles)) {
+            //         return true;
+            //     }
+            // },
+            self::PUBLIC => function () {
+                return true;
+            },
+        };
+    }
+}
