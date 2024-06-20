eda4fad5774b5f38c76765a4b56cfccf09f139f2
Heikki Vihersalo <heikki@vihersalo.fi>
2024-06-20 12:03:27 +0300

    moved to theme folder

---
+++ inc/theme/api/interfaces/interface-http-response.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kotisivu\BlockTheme\Api;
+
+defined('ABSPATH') or die();
+
+interface HTTP_Response_Interface {
+    public function values();
+    public function get_type();
+    public function get_message();
+    public function get_http_status();
+}
