2fc68291aa86cec979e9621a4491bcaa22dccd10
Heikki Vihersalo <heikki@vihersalo.fi>
2024-06-20 12:04:50 +0300

    added composer support

---
+++ inc/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
+    return;
+};
+
+/**
+ * Include autoload.php to load all dependencies.
+ */
+require_once dirname(__DIR__) . '/vendor/autoload.php';
