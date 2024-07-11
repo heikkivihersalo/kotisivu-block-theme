a170ee4c9f18e8b5bdea4b0da010a169e6fe561d
Heikki Vihersalo <heikki@vihersalo.fi>
2024-07-11 19:56:17 +0300

    converted to typescript

---
+++ src/block-library/parts/part-language-picker/edit.tsx
@@ -0,0 +1,37 @@
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+import ServerSideRender from '@wordpress/server-side-render';
+
+/**
+ * Internal dependencies
+ */
+import './editor.css';
+
+/**
+ * Block edit function
+ * @param {Object} props Properties
+ * @param {Object} props.attributes Block attributes
+ * @param {string} props.className Block class name
+ * @return {JSX.Element} React component
+ */
+export default function Edit({
+	attributes,
+	className,
+}: {
+	attributes: Record<string, any>;
+	className: string;
+}): JSX.Element {
+	const blockProps = useBlockProps();
+
+	return (
+		<div {...blockProps}>
+			<ServerSideRender
+				block="ksd/part-language-picker"
+				attributes={attributes}
+				className={className}
+			/>
+		</div>
+	);
+}
