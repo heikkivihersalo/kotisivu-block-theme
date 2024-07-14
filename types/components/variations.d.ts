5d00c56ab957ebbcc6255a149c4e4709b7e72385
Heikki Vihersalo <heikki@vihersalo.fi>
2024-07-14 13:58:59 +0300

    moved to folder for better organization

---
+++ types/components/variations.d.ts
@@ -0,0 +1,35 @@
+declare module '@components/variations' {
+	import { ComponentType } from 'react';
+
+	/**
+	 * Set types for component props
+	 */
+	type VariationPickerProps = {
+		blockName: string;
+		setAttributes: (newAttributes: Record<string, any>) => void;
+	};
+
+	type Variation = {
+		name: string;
+		title: string;
+		icon: string;
+		scope: string;
+		innerBlocks: Array<Array<string | Record<string, any>>>;
+		attributes: {
+			blockClass: string;
+			childTemplate?: Array<string | Record<string, any>>;
+			style?: Record<string, any>;
+			templateLock?: 'all';
+			width?: 'var(--wp--custom--wide-size)';
+		};
+	};
+
+	export type { VariationPickerProps, Variation };
+
+	/**
+	 * Set types for component
+	 */
+	const VariationPicker: ComponentType<VariationPickerProps>;
+
+	export { VariationPicker };
+}
