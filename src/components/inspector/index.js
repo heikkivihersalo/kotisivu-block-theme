372a68ae1586270bf7a2fbe78ae7c4fa0afaca51
Heikki Vihersalo <heikki@vihersalo.fi>
2024-07-20 12:41:09 +0300

    migrated link controls to typescript

--- src/components/inspector/index.js
+++ src/components/inspector/index.js
@@ -2,4 +2,5 @@ export * from './lib/ariaLabelControls';
 export * from './lib/backgroundColorControls';
 export * from './lib/widthControls';
 export * from './lib/gridAlignControls';
+export * from './lib/linkControls';
 export * from './lib/gapControls';
