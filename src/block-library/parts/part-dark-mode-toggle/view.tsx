4682bdc9c6cb76b973490229836428922b5bb001
Heikki Vihersalo <heikki@vihersalo.fi>
2024-07-22 20:13:28 +0300

    removed unnecessary import

--- src/block-library/parts/part-dark-mode-toggle/view.tsx
+++ src/block-library/parts/part-dark-mode-toggle/view.tsx
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import domReady from '@wordpress/dom-ready';
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
