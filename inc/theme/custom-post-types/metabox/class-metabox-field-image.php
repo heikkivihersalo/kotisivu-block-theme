0eb6c3453ed0d368b94491fe988fef7c32b7f6bd
Heikki Vihersalo <heikki@vihersalo.fi>
2024-07-22 20:08:00 +0300

    fix button visibility

--- inc/theme/custom-post-types/metabox/class-metabox-field-image.php
+++ inc/theme/custom-post-types/metabox/class-metabox-field-image.php
@@ -24,18 +24,17 @@ class ImageField extends MetaboxField implements MetaboxFieldInterface {
 			</th>
 			<td>
 				<?php $img_src = wp_get_attachment_image_src( get_post_meta( $this->post->ID, $this->id, true ), 'full' ); ?>
-
 				<div class="image-uploader">
-					<input class="metabox__input-field metabox__input-field--image image-uploader__input-field" id="<?php echo $this->id; ?>" type="hidden" name="<?php echo $this->id; ?>" value="<?php echo $this->get_value(); ?>" />
-					<img src="<?php echo $img_src[0]; ?>" style="width: 300px;" alt="" class="image-uploader__preview<?php echo is_array( $img_src ) === '' ? ' is-visually-hidden--no-tag' : ''; ?>" />
+					<input class="image-uploader__input" id="<?php echo $this->id; ?>" type="hidden" name="<?php echo $this->id; ?>" value="<?php echo $this->get_value(); ?>" />
+					<img src="<?php echo $img_src[0]; ?>" style="width: 300px;" alt="" class="image-uploader__preview<?php echo false === $img_src ? ' hide-image-uploader' : ''; ?>" />
 					<div class="image-uploader__buttons">
-						<button class="image-uploader__button image-uploader__button--choose<?php echo is_array( $img_src ) === '' ? ' ' : ' is-visually-hidden--no-tag'; ?>">Choose image</button>
-						<button class="image-uploader__button image-uploader__button--remove<?php echo is_array( $img_src ) === '' ? ' is-visually-hidden--no-tag' : ''; ?>">Remove Image</button>
+						<button class="image-uploader__button image-uploader__button--choose<?php echo false === $img_src ? '' : ' hide-image-uploader'; ?>"><?php _e( 'Choose image', 'kotisivu-block-theme' ); ?></button>
+						<button class="image-uploader__button image-uploader__button--remove<?php echo false === $img_src ? ' hide-image-uploader' : ''; ?>"><?php _e( 'Remove Image', 'kotisivu-block-theme' ); ?></button>
 					</div>
 				</div>
 			</td>
 		</tr>
-		
+
 		<?php
 		return ob_get_clean();
 	}
