71c2e3720b0a080db7af334c88cb01212bcd968c
Heikki Vihersalo <heikki@vihersalo.fi>
2024-07-24 21:08:57 +0300

    converted to typescript

---
+++ src/block-library/core/buttons/variations.ts
@@ -0,0 +1,16 @@
+import type { BlockStyle } from 'core-blocks';
+
+const variations: BlockStyle[] = [
+	{
+		name: 'outline',
+		label: 'Outline',
+		isDefault: false,
+	},
+	{
+		name: 'text',
+		label: 'Text',
+		isDefault: false,
+	},
+];
+
+export default variations;
