665294f02d2887cd75ca3c3b03ca3d5737a35c6f
Heikki Vihersalo <heikki@vihersalo.fi>
2024-07-24 17:52:38 +0300

    added dotenv

--- bootstrap.php
+++ bootstrap.php
@@ -8,3 +8,13 @@ if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
  * Include autoload.php to load all dependencies.
  */
 require_once __DIR__ . '/vendor/autoload.php';
+
+/**
+ * Include Dotenv library to pull config options from .env file.
+ */
+if ( ! class_exists( 'Dotenv\Dotenv' ) ) {
+	return;
+}
+
+$dotenv = Dotenv\Dotenv::createImmutable( __DIR__ );
+$dotenv->load();
