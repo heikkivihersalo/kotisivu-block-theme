7f8710bbcc94a9c222bea03f3b1118690d904354
Heikki Vihersalo <heikki@vihersalo.fi>
2024-07-28 23:45:04 +0300

    get functions php file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,10 @@
 /**
  * PHPUnit bootstrap file.
  */
-define('TEST_DIRECTORY', getenv( 'WP_TESTS_DIR' ));
-define('POLYFILL_DIRECTORY', getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' ));
+define( 'TEST_DIRECTORY', getenv( 'WP_TESTS_DIR' ) );
+define( 'POLYFILL_DIRECTORY', getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' ) );
 
-if ( !defined( 'WP_DEFAULT_THEME' ) ) {
+if ( ! defined( 'WP_DEFAULT_THEME' ) ) {
 	define( 'WP_DEFAULT_THEME', 'kotisivu-block-theme' );
 }
 
@@ -21,21 +21,27 @@ require 'vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 /**
  * Load the WordPress tests library.
  */
-require_once TEST_DIRECTORY . "/includes/functions.php";
+require_once TEST_DIRECTORY . '/includes/functions.php';
 
 /**
  * Start up the WP testing environment.
  */
-require TEST_DIRECTORY . "/includes/bootstrap.php";
+require TEST_DIRECTORY . '/includes/bootstrap.php';
 
 /**
  * Create application password for API testing.
  */
 \WP_Application_Passwords::delete_all_application_passwords( 1 );
 
-$app_pass = \WP_Application_Passwords::create_new_application_password( 
-	1, array( 'name' => 'Test Application Password' ) 
+$app_pass = \WP_Application_Passwords::create_new_application_password(
+	1,
+	array( 'name' => 'Test Application Password' )
 );
+
+/**
+ * Load the theme functions file.
+ */
+require_once dirname( __DIR__, 1 ) . '/functions.php';
 
 /**
  * Set application password and user for API testing.
@@ -45,10 +51,4 @@ define( 'APP_PASS', $app_pass[0] );
 define( 'ADMIN_USER', 'admin' );
 define( 'ADMIN_PASS', 'password' );
 
-if (!defined('SITE_PATH')) {
-	define('SITE_PATH', dirname(__DIR__, 1));
-}
-
-if (!defined('SITE_URL')) {
-	define('SITE_URL', 'host.docker.internal:8889');
-}
+define( 'SITE_URL', 'host.docker.internal:8889' );
