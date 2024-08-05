641019a635172eccdf4db8309f58e2f374c80556
Heikki Vihersalo <heikki@vihersalo.fi>
2024-08-05 15:00:44 +0300

    removed selected text box

--- src/ai/components/form/SelectionControl.tsx
+++ src/ai/components/form/SelectionControl.tsx
@@ -12,42 +12,30 @@ import styles from './SelectionControl.module.css';
  * Form component
  * @param {Object} props - Component props
  * @param {string} props.selectedText - Form children
- * @return {JSX.Element} Form component
+ * @return {JSX.Element | null} Form component
  */
 const SelectionControl = ({
 	selectedText,
 }: {
 	selectedText: string;
-}): JSX.Element => {
+}): JSX.Element | null => {
 	return (
-		<>
-			{selectedText !== '' ? (
-				<>
-					<div className={styles.selectedContainer}>
-						<span className={styles.selectedLabel}>
-							{__('Selected text', 'kotisivu-block-theme')}
-						</span>
-						<p className={styles.selectedText}>{selectedText}</p>
-					</div>
-				</>
-			) : null}
-			<div
-				className={styles.checkboxContainer}
-				data-disabled={selectedText === ''}
-			>
-				<input
-					id="use-selected"
-					className={styles.checkboxInput}
-					type="checkbox"
-					defaultChecked={selectedText !== ''}
-					disabled={selectedText === ''}
-					name="use-selected"
-				/>
-				<label htmlFor="use-selected">
-					{__('Use text selection', 'kotisivu-block-theme')}
-				</label>
-			</div>
-		</>
+		<div
+			className={styles.checkboxContainer}
+			data-disabled={selectedText === ''}
+		>
+			<input
+				id="use-selected"
+				className={styles.checkboxInput}
+				type="checkbox"
+				defaultChecked={selectedText !== ''}
+				disabled={selectedText === ''}
+				name="use-selected"
+			/>
+			<label htmlFor="use-selected">
+				{__('Use text selection', 'kotisivu-block-theme')}
+			</label>
+		</div>
 	);
 };
 
