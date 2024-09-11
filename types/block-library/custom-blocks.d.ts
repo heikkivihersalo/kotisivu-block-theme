219f3704441a9c5cc54e864d968aae0edceb3b1b
Heikki Vihersalo <heikki@vihersalo.fi>
2024-09-11 11:46:09 +0300

    added index-tsx block types

--- types/block-library/custom-blocks.d.ts
+++ types/block-library/custom-blocks.d.ts
@@ -1,12 +1,26 @@
 declare module '@custom-blocks' {
-	import { BlockStyles } from '@utils';
+	import type {
+		BlockEditProps,
+		BlockSaveProps,
+		BlockConfiguration,
+	} from '@wordpress/blocks';
+	import { Variation } from '@components/variations';
 
-	interface ContainerBlockProps extends Record<string, any> {
-		className: string;
-		style: BlockStyles;
-		'aria-label': any;
-		'aria-labelledby': any;
-	}
+	type BlockEdit = ({
+		attributes,
+		setAttributes,
+	}: BlockEditProps<Record<string, any>>) => JSX.Element;
 
-	export { ContainerBlockProps };
+	type BlockSave = ({
+		attributes,
+	}: BlockSaveProps<Record<string, any>>) => JSX.Element | null;
+
+	type BlockConfig = BlockConfiguration & {
+		save: BlockSave;
+		edit: BlockEdit;
+		variations?: Array<Variation>;
+		getEditWrapperProps?: () => { 'data-align': string };
+	};
+
+	export type { BlockEdit, BlockSave, BlockConfig };
 }
