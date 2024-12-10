efd6c54568f24e51681e953905bdb17d572ff64e
Heikki Vihersalo <heikki@vihersalo.fi>
2024-12-10 16:06:58 +0200

    null check for hooks

--- inc/theme/Common/Loader.php
+++ inc/theme/Common/Loader.php
@@ -73,11 +73,11 @@ class Loader {
 	 * Add a new action to the collection to be registered with WordPress.
 	 *
 	 * @since    2.0.0
-	 * @param    string $hook             The name of the WordPress action that is being registered.
-	 * @param    object $component        A reference to the instance of the object on which the action is defined.
-	 * @param    string $callback         The name of the function definition on the $component.
-	 * @param    int    $priority         Optional. The priority at which the function should be fired. Default is 10.
-	 * @param    int    $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1.
+	 * @param    string      $hook             The name of the WordPress action that is being registered.
+	 * @param    object|null $component        A reference to the instance of the object on which the action is defined.
+	 * @param    string      $callback         The name of the function definition on the $component.
+	 * @param    int         $priority         Optional. The priority at which the function should be fired. Default is 10.
+	 * @param    int         $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1.
 	 */
 	public function add_action( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->actions = $this->add( $this->actions, $hook, $component, $callback, $priority, $accepted_args );
@@ -100,11 +100,11 @@ class Loader {
 	 * Add a new filter to the collection to be registered with WordPress.
 	 *
 	 * @since    2.0.0
-	 * @param    string $hook             The name of the WordPress filter that is being registered.
-	 * @param    object $component        A reference to the instance of the object on which the filter is defined.
-	 * @param    string $callback         The name of the function definition on the $component.
-	 * @param    int    $priority         Optional. The priority at which the function should be fired. Default is 10.
-	 * @param    int    $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1
+	 * @param    string      $hook             The name of the WordPress filter that is being registered.
+	 * @param    object|null $component        A reference to the instance of the object on which the filter is defined.
+	 * @param    string      $callback         The name of the function definition on the $component.
+	 * @param    int         $priority         Optional. The priority at which the function should be fired. Default is 10.
+	 * @param    int         $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1
 	 */
 	public function add_filter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->filters = $this->add( $this->filters, $hook, $component, $callback, $priority, $accepted_args );
@@ -156,19 +156,37 @@ class Loader {
 	 */
 	public function run() {
 		foreach ( $this->remove_filters as $hook ) {
-			remove_filter( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'] );
+			remove_filter(
+				$hook['hook'],
+				$hook['component'] !== null ? array( $hook['component'], $hook['callback'] ) : $hook['callback'],
+				$hook['priority']
+			);
 		}
 
 		foreach ( $this->remove_actions as $hook ) {
-			remove_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'] );
+			remove_action(
+				$hook['hook'],
+				$hook['component'] !== null ? array( $hook['component'], $hook['callback'] ) : $hook['callback'],
+				$hook['priority']
+			);
 		}
 
 		foreach ( $this->filters as $hook ) {
-			add_filter( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+			add_filter(
+				$hook['hook'],
+				$hook['component'] !== null ? array( $hook['component'], $hook['callback'] ) : $hook['callback'],
+				$hook['priority'],
+				$hook['accepted_args']
+			);
 		}
 
 		foreach ( $this->actions as $hook ) {
-			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+			add_action(
+				$hook['hook'],
+				$hook['component'] !== null ? array( $hook['component'], $hook['callback'] ) : $hook['callback'],
+				$hook['priority'],
+				$hook['accepted_args']
+			);
 		}
 	}
 }
