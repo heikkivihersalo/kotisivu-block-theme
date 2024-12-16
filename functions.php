107ab84d2e008b9f1cbb5a488e9b4a69653c6fef
Heikki Vihersalo <heikki@vihersalo.fi>
2024-12-16 13:52:33 +0200

    add activator and deactivator hooks

--- functions.php
+++ functions.php
@@ -8,6 +8,9 @@
 
 namespace Kotisivu\BlockTheme;
 
+use Kotisivu\BlockTheme\Theme\Common\Activator;
+use Kotisivu\BlockTheme\Theme\Common\Deactivator;
+
 /**
  * If this file is called directly, abort.
  */
@@ -36,26 +39,24 @@ define( 'SITE_URI', get_template_directory_uri() );
 
 require SITE_PATH . '/inc/Theme.php';
 
-// /**
-// * The code that runs during plugin activation.
-// * This action is documented in includes/class-gutenberg-native-ai-activator.php
-// */
-// function activate_gutenberg_native_ai() {
-// require_once plugin_dir_path( __FILE__ ) . 'includes/class-activator.php';
-// Activator::activate();
-// }
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-gutenberg-native-ai-activator.php
+ */
+function activate_gutenberg_native_ai() {
+	Activator::activate();
+}
 
-// /**
-// * The code that runs during plugin deactivation.
-// * This action is documented in includes/class-gutenberg-native-ai-deactivator.php
-// */
-// function deactivate_gutenberg_native_ai() {
-// require_once plugin_dir_path( __FILE__ ) . 'includes/class-deactivator.php';
-// Deactivator::deactivate();
-// }
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-gutenberg-native-ai-deactivator.php
+ */
+function deactivate_gutenberg_native_ai() {
+	Deactivator::deactivate();
+}
 
-// register_activation_hook( __FILE__, __NAMESPACE__ . '\activate_gutenberg_native_ai' );
-// register_deactivation_hook( __FILE__, __NAMESPACE__ . '\deactivate_gutenberg_native_ai' );
+register_activation_hook( __FILE__, __NAMESPACE__ . '\activate_gutenberg_native_ai' );
+register_deactivation_hook( __FILE__, __NAMESPACE__ . '\deactivate_gutenberg_native_ai' );
 
 /**
  * Begins execution of the plugin.
