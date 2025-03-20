01298f2671fe721bbad93ecafa0e5e91a4da1536
Heikki Vihersalo <heikki@vihersalo.fi>
2025-03-20 17:47:01 +0200

    remove unncessary bootstrap

--- bootstrap/theme.php
+++ bootstrap/theme.php
@@ -4,30 +4,13 @@ declare(strict_types=1);
 
 namespace Kotisivu\BlockTheme;
 
-use HeikkiVihersalo\BlockThemeCore;
-use HeikkiVihersalo\BlockThemeCore\Theme;
-use HeikkiVihersalo\BlockThemeCore\Theme\Common\Utils\Helpers as ThemeHelpers;
+use Vihersalo\BlockThemeCore\Application;
 
 require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-base.php';
 require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-direct.php';
 
 /**
- * Include theme.php to get theme defaults.
+ * Bind the loader to the application.
  */
-$theme = ThemeHelpers::load_config( dirname( __DIR__ ) . '/config/theme.php' );
-
-foreach ( $theme as $key => $value ) {
-	define( 'SITE_' . strtoupper( $key ), $value );
-}
-
-/**
- * Run the theme.
- */
-$theme = new Theme(
-	ThemeHelpers::load_config( dirname( __DIR__ ) . '/config/enqueue.php' ),
-	ThemeHelpers::load_config( dirname( __DIR__ ) . '/config/excerpt.php' ),
-	ThemeHelpers::load_config( dirname( __DIR__ ) . '/config/images.php' ),
-	ThemeHelpers::load_config( dirname( __DIR__ ) . '/config/navigation.php' )
-);
-
-$theme->run();
+return Application::configure()
+	->boot();
