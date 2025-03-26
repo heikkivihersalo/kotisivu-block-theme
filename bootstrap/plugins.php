7ba55e217c16f4bcdbfd67da22e0eefa7d61f02f
Heikki Vihersalo <heikki@vihersalo.fi>
2025-03-26 20:13:57 +0200

    delete as unnecessary

--- bootstrap/plugins.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Kotisivu\BlockTheme;
-
-use HeikkiVihersalo\CustomPostTypes\CPTLoader;
-
-( new CPTLoader( SITE_PATH, SITE_URI, __NAMESPACE__ ) )->run();
