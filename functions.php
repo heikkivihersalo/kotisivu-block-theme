cbcaa27f7cf537414bdba659779289b54ffac46e
Heikki Vihersalo <heikki@vihersalo.fi>
2025-03-26 20:12:49 +0200

    migrate to laravel coding standards

--- functions.php
+++ functions.php
@@ -1,21 +1,14 @@
 <?php
-/**
- * Theme functions
- *
- * @package Kotisivu\BlockTheme
- * @since 2.0.0
- */
-
-namespace Kotisivu\BlockTheme;
 
 /**
  * Load all theme related files and classes
  *
  * !Do not remove these lines!
  */
+
 require_once __DIR__ . '/bootstrap/vendor.php';
 require_once __DIR__ . '/bootstrap/theme.php';
 
 /**
- * Add your custom code here if needed or use the bootstrap files
+ * Add your custom code here if needed or use the app folder (app/) for more organized code.
  */
