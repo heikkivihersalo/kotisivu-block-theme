23d04a2d693a5ed0df68af85050d7993b64b7cf3
Heikki Vihersalo <heikki@vihersalo.fi>
2025-03-26 20:15:47 +0200

    remove unnecessary code

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
-use Vihersalo\Core\Bootstrap\Application;
-use Vihersalo\Core\Api\Router;
 use Vihersalo\Core\Api\Enums\Permission;
-
 use App\Http\Controllers\HelloController;
-
 use Vihersalo\Core\Support\Facades\Route;
 
-// Application::getInstance()->make(Router::class)->get(
-// '/hello',
-// [HelloController::class, 'index']
-// )->auth(Permission::PUBLIC);
-
 Route::get(
-	'/hello',
-	[ HelloController::class, 'index' ]
-)->auth( Permission::PUBLIC );
+    '/hello',
+    [ HelloController::class, 'index' ]
+)->auth(Permission::PUBLIC);
