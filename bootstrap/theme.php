8c00489767969957a78b77b3ce0f8f323190c0d0
Heikki Vihersalo <heikki@vihersalo.fi>
2025-03-30 21:26:09 +0300

    add handler

--- bootstrap/theme.php
+++ bootstrap/theme.php
@@ -2,9 +2,16 @@
 
 declare(strict_types=1);
 
-return Vihersalo\Core\Foundation\Application::configure()
+use App\Handlers\RenderBlockHandler;
+use Vihersalo\Core\Foundation\Application;
+use Vihersalo\Core\Support\Collections\HandlerCollection;
+
+return Application::configure()
     ->withApi(
         routePath: __DIR__ . '/../routes/api.php',
-        routeNamespace: 'api/v1',
+        routeNamespace: 'api/v1', // This will make the routes available under `/wp-json/api/v1`
     )
+    ->withHandlers(function (HandlerCollection $handlers) {
+        $handlers->add(RenderBlockHandler::class);
+    })
     ->boot();
