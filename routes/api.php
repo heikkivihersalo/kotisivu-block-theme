9a5ce1a166e0f9a250afd51cd29a121bd487879e
Heikki Vihersalo <heikki@vihersalo.fi>
2025-03-31 22:38:12 +0300

    add settings api

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,27 @@
 
 use App\Enums\Permission;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\Settings\TagmanagerController;
+use App\Http\Controllers\Settings\ContactController;
+use App\Http\Controllers\Settings\SocialAccountController;
 use Vihersalo\Core\Support\Facades\Route;
 
-Route::get(
-    '/hello',
-    [ HelloController::class, 'index' ]
-)->auth(Permission::PUBLIC);
+Route::controller(HelloController::class)->group(function () {
+    Route::get('/hello', 'index');
+    Route::get('/apina', 'index');
+});
+
+Route::controller(TagmanagerController::class)->group(function () {
+    Route::get('/settings/tagmanager', 'index')->auth(Permission::PUBLIC);
+    Route::get('/settings/tagmanager', 'update')->auth(Permission::ADMIN);
+});
+
+Route::controller(ContactController::class)->group(function () {
+    Route::get('/settings/contact', 'index')->auth(Permission::PUBLIC);
+    Route::get('/settings/contact', 'update')->auth(Permission::ADMIN);
+});
+
+Route::controller(SocialAccountController::class)->group(function () {
+    Route::get('/settings/socials', 'index')->auth(Permission::PUBLIC);
+    Route::get('/settings/socials', 'update')->auth(Permission::ADMIN);
+});
