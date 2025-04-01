110ad65e728a07963ee1ac5ee9fc94c4db2c06dc
Heikki Vihersalo <heikki@vihersalo.fi>
2025-04-01 12:02:03 +0300

    use common collection for handlers

--- bootstrap/theme.php
+++ bootstrap/theme.php
@@ -3,15 +3,14 @@
 declare(strict_types=1);
 
 use App\Handlers\RenderBlockHandler;
-use Vihersalo\Core\Foundation\Application;
-use Vihersalo\Core\Support\Collections\HandlerCollection;
+use Vihersalo\Core\Support\Collection;
 
-return Application::configure()
+return \Vihersalo\Core\Foundation\Application::configure()
     ->withApi(
         routePath: __DIR__ . '/../routes/api.php',
         routeNamespace: 'api/v1', // This will make the routes available under `/wp-json/api/v1`
     )
-    ->withHandlers(function (HandlerCollection $handlers) {
+    ->withHandlers(function (Collection $handlers) {
         $handlers->add(RenderBlockHandler::class);
     })
     ->boot();
