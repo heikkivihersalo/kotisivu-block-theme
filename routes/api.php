ac59b4c4b1e584f3607376f31d8934262374522c
Heikki Vihersalo <heikki@vihersalo.fi>
2025-04-01 13:21:07 +0300

    fix paths

--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,27 @@
 <?php
 
 use App\Enums\Permission;
-use App\Http\Controllers\HelloController;
 use App\Http\Controllers\Settings\TagmanagerController;
 use App\Http\Controllers\Settings\ContactController;
 use App\Http\Controllers\Settings\SocialAccountController;
+use App\Http\Controllers\Settings\CacheController;
 use Vihersalo\Core\Support\Facades\Route;
-
-Route::controller(HelloController::class)->group(function () {
-    Route::get('/hello', 'index');
-    Route::get('/apina', 'index');
-});
 
 Route::controller(TagmanagerController::class)->group(function () {
     Route::get('/settings/tagmanager', 'index')->auth(Permission::PUBLIC);
-    Route::get('/settings/tagmanager', 'update')->auth(Permission::ADMIN);
+    Route::post('/settings/tagmanager', 'update')->auth(Permission::ADMIN);
 });
 
 Route::controller(ContactController::class)->group(function () {
     Route::get('/settings/contact', 'index')->auth(Permission::PUBLIC);
-    Route::get('/settings/contact', 'update')->auth(Permission::ADMIN);
+    Route::post('/settings/contact', 'update')->auth(Permission::ADMIN);
 });
 
 Route::controller(SocialAccountController::class)->group(function () {
     Route::get('/settings/socials', 'index')->auth(Permission::PUBLIC);
-    Route::get('/settings/socials', 'update')->auth(Permission::ADMIN);
+    Route::post('/settings/socials', 'update')->auth(Permission::ADMIN);
+});
+
+Route::controller(CacheController::class)->group(function () {
+    Route::post('/settings/cache/purge', 'purge')->auth(Permission::ADMIN);
 });
