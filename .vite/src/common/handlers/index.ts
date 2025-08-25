8d68cd475b7045dc945893e7b18d9a6ab40d4b4f
Heikki Vihersalo <heikki@vihersalo.fi>
2025-08-25 23:38:21 +0300

    add asset handler

--- .vite/src/common/handlers/index.ts
+++ .vite/src/common/handlers/index.ts
@@ -7,3 +7,4 @@
 export { CSS_Handler } from './CSS_Handler.ts';
 export { JS_Handler } from './JS_Handler.ts';
 export { PHP_Handler } from './PHP_Handler.ts';
+export { AssetHandler } from './AssetHandler.ts';
