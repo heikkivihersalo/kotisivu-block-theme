2ce0600502875f1d9bd90f3c19fc4aec19f45d89
Heikki Vihersalo <heikki@vihersalo.fi>
2025-08-25 23:16:58 +0300

    rename

--- .vite/src/common/processors/index.ts
+++ .vite/src/common/processors/index.ts
@@ -6,4 +6,4 @@
 
 export { CssProcessor } from './CssProcessor.ts';
 export { ScriptProcessor } from './ScriptProcessor.ts';
-export { processPhp, processPhpFiles } from './_phpProcessor.ts';
+export { processPhp, processPhpFiles } from './PhpProcessor.ts';
